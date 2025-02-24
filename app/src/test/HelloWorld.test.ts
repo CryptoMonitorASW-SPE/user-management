f1d16e2be2a9f39c85e75f4d00e951ba8ebfe776
Luca Tonelli <luca.tonelli11@studio.unibo.it>
2025-02-24 18:38:51 +0100

    chore: remove HelloWorld test suite

--- app/src/test/HelloWorld.test.ts
+++
@@ -1,8 +0,0 @@
-import { expect } from 'chai'
-
-describe('HelloWorld Test Suite', () => {
-  it('should return correct greeting', () => {
-    const greeting = 'Hello World!'
-    expect(greeting).to.equal('Hello World!')
-  })
-})
